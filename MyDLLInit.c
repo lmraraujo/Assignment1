5c7d00b6f3b21ddf73fd74c6c178fbac1a39e907
Joao Modesto <jbernardomm@ua.pt>
2024-03-08 18:30:44 +0000

    so p ver se funciona

--- MyDLLInit.c
+++ MyDLLInit.c
@@ -5,8 +5,8 @@
 void MyDLLInit(DoublyLinkedList* dll) {
     // Inicializa os membros da estrutura DoublyLinkedList
     dll->size = 0; 
-    dll->head = -1; // -1 -> list is empty
-    dll->tail = -1; // -1 -> list is empty
+    dll->head = NULL; // -1 -> list is empty
+    dll->tail = NULL; // -1 -> list is empty
 
     // Initialize all nodes as free 
     for (int i = 0; i < MAX_ELEMENTS; i++) {
